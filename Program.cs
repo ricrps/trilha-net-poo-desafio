3e0c1d5b5e4ef369df4fb28440460205d3514318
Ricardo Pereira <ricardo.pereira@altagenetics.com>
2024-08-20 00:17:14 -0300

    - resolução do desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia("99999-9999", "N95", "A0988-5823", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Twitch");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iPhone = new Iphone("8888-88889", "X", "A0888-8888", 64);
+iPhone.Ligar();
+iPhone.InstalarAplicativo("Netflix");
